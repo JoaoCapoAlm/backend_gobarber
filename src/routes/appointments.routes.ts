776c90ab1f34f83decff4fd5109b67586d25cd19
João Pedro <jpcapoani98@gmail.com>
2021-01-11 10:26:33 -0300

    Criação de registro de usuário

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,6 +1,6 @@
+import { parseISO } from 'date-fns';
 import { Router } from 'express';
 import { getCustomRepository } from 'typeorm';
-import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentServices';
 
@@ -15,12 +15,12 @@ appointmentsRouter.get('/', async (request, response) =>{
 
 appointmentsRouter.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
     const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({ date: parsedDate, provider });
+    const appointment = await createAppointment.execute({ date: parsedDate, provider_id });
 
     return response.json(appointment);
   } catch (err) {
