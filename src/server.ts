5052bdcd0d0ab14cddc8bb5b7678c0c4ba33e412
João Pedro <jpcapoani98@gmail.com>
2021-01-11 22:47:45 -0300

    Servido arquivos estáticos

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,15 @@
-import 'reflect-metadata'
-
 import express from 'express';
+import 'reflect-metadata';
+import uploadConfig from './config/uploads';
+import './database';
 import routes from './routes';
 
-import './database';
+
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
